80a2dca3a8c51f3acd3601d546cc8226440f065c
Rohan Vasudev Ginde <122499715+rohanginde@users.noreply.github.com>
2023-10-18 19:59:39 -0400

    Assignment5 added (#14)
    * add app.js file for healthz endpoint (#2)
    
    * code updated (#12)
    
    * Create integrationTestyml
    
    * add app.js file for healthz endpoint
    
    * Update blank.yml
    
    * Update blank.yml
    
    * add app.js file for healthz endpoint (#3)
    
    * Update app.js
    
    * Update app.js
    
    * Update blank.yml
    
    * Update blank.yml
    
    * add app.js file for healthz endpoint (#6)
    
    * Update blank.yml
    
    * Update app.js
    
    * Update blank.yml
    
    * Update blank.yml
    
    * Update blank.yml
    
    * Update blank.yml
    
    * Update blank.yml
    
    * code updated
    
    * Delete .github/workflows directory
    
    * Assignment3 (#14)
    
    * Create integrationTestyml
    
    * add app.js file for healthz endpoint
    
    * Update blank.yml
    
    * Update blank.yml
    
    * add app.js file for healthz endpoint (#3)
    
    * Update app.js
    
    * Update app.js
    
    * Update blank.yml
    
    * Update blank.yml
    
    * add app.js file for healthz endpoint (#6)
    
    * Update blank.yml
    
    * Update app.js
    
    * Update blank.yml
    
    * Update blank.yml
    
    * Update blank.yml
    
    * Update blank.yml
    
    * Update blank.yml
    
    * code updated
    
    * removed DS_store
    
    * removed DS_store
    
    * removed node version
    
    * Sub (#19)
    
    * code updated
    
    * removed DS_store
    
    * removed DS_store
    
    * removed node version
    
    * Rename Assignment.js to assignment.js
    
    * Sub2 (#20)
    
    * code updated
    
    * removed DS_store
    
    * removed DS_store
    
    * removed node version
    
    * changed
    
    * changed
    
    * Rename Assignment.js to assignment.js
    
    * Rename User.js to user.js
    
    * Sub2 (#22)
    
    * code updated
    
    * removed DS_store
    
    * removed DS_store
    
    * removed node version
    
    * changed
    
    * changed
    
    * Rename Assignment.js to assignment.js
    
    * Rename User.js to user.js
    
    * Update assignmentService.js
    
    * Update commonService.js
    
    * Sub2 (#23)
    
    * code updated
    
    * removed DS_store
    
    * removed DS_store
    
    * removed node version
    
    * changed
    
    * changed
    
    * Rename Assignment.js to assignment.js
    
    * Rename User.js to user.js
    
    * Update assignmentService.js
    
    * Update commonService.js
    
    * Update app.yml
    
    * Sub2 (#24)
    
    * code updated
    
    * removed DS_store
    
    * removed DS_store
    
    * removed node version
    
    * changed
    
    * changed
    
    * Rename Assignment.js to assignment.js
    
    * Rename User.js to user.js
    
    * Update assignmentService.js
    
    * Update commonService.js
    
    * Update app.yml
    
    * Update app.yml
    
    * Update app.yml
    
    * Update app.yml
    
    * Update app.yml
    
    * Update app.yml
    
    * Update app.yml
    
    * Update app.yml
    
    * Update app.yml
    
    * Update app.yml
    
    * Update app.js
    
    * Update app.yml
    
    * Update integration.test.js
    
    * Update integration.test.js
    
    * Update integration.test.js
    
    * added new workflow
    
    * edited packer
    
    * build-ami added
    
    * packer file modified
    
    * added new workflow
    
    * edited packer
    
    * build-ami added
    
    * packer file modified
    
    * build ami updated
    
    * Assignment5 (#45)
    
    * added new workflow
    
    * edited packer
    
    * build-ami added
    
    * packer file modified
    
    * build ami updated
    
    * build ami updated again
    
    * Assignment5 (#46)
    
    * added new workflow
    
    * edited packer
    
    * build-ami added
    
    * packer file modified
    
    * build ami updated
    
    * build ami updated again
    
    * shell script updated
    
    * Assignment5 (#47)
    
    * added new workflow
    
    * edited packer
    
    * build-ami added
    
    * packer file modified
    
    * build ami updated
    
    * build ami updated again
    
    * shell script updated
    
    * shell script updated
    
    * Assignment5 (#48)
    
    * added new workflow
    
    * edited packer
    
    * build-ami added
    
    * packer file modified
    
    * build ami updated
    
    * build ami updated again
    
    * shell script updated
    
    * shell script updated
    
    * shell updated
    
    * Assignment5 (#49)
    
    * added new workflow
    
    * edited packer
    
    * build-ami added
    
    * packer file modified
    
    * build ami updated
    
    * build ami updated again
    
    * shell script updated
    
    * shell script updated
    
    * shell updated
    
    * shell updated

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+ 
+
+
+sudo apt-get update
+
+ 
+
+
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs npm
+
+ 
+
+# Install MariaDB server and client
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mariadb-server mariadb-client
+
+ 
+
+# Check Node.js version
+
+sudo DEBIAN_FRONTEND=noninteractive node -v
+
+ 
+
+
+
+sudo mysql --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'; FLUSH PRIVILEGES;"
+
+
+npm -v
+
+ 
+
+# Install unzip
+
+sudo DEBIAN_FRONTEND=noninteractive apt install -y unzip
+
+
+# Unzip the WebAppRenamed file to the WebApp directory
+
+sudo unzip WebAppRenamed -d WebApp
+ 
+
+
