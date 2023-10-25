b0aa439e97efcd8049db4488e5e387fad98ef9c9
Rohan Vasudev Ginde <ginde.r@northeastern.edu>
2023-10-25 19:20:47 -0400

    final

--- automatic-start.sh
+++ automatic-start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-cd WebApp || exit
 
 sudo cp /home/admin/nodeapp.service /lib/systemd/system/nodeapp.service
 
 
-# systemctl daemon-reload
-# sudo systemctl enable nodeapp
-# sudo systemctl start nodeapp
-# sudo systemctl restart nodeapp
+sudo systemctl daemon-reload
+sudo systemctl enable nodeapp
+sudo systemctl start nodeapp
+sudo systemctl restart nodeapp
