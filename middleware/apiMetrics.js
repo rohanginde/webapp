15c7ffc8a431ca3fef6503fc0f980601c393fc1f
Rohan Vasudev Ginde <ginde.r@northeastern.edu>
2023-11-22 10:06:59 -0500

    Assignment9

--- middleware/apiMetrics.js
+++ middleware/apiMetrics.js
@@ -1,14 +1,12 @@
-// Import the 'hot-shots' StatsD client
+
 import StatsD from 'hot-shots';
 
 const statsd = new StatsD();
 
-// Middleware for tracking API calls and sending custom metrics
-const trackAPICalls = (req, res, next) => {
-  // Your API logic here
 
-  // Track the API call by incrementing a custom metric
-  const apiName = req.path; // You can adjust this to track the specific API
+const trackAPICalls = (req, res, next) => {
+
+  const apiName = req.path; 
   statsd.increment(`api.${apiName}.calls`);
 
   next();
